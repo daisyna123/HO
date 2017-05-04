4f2033a75df17adf676b3e6f6ad090c71a56aaa0
daisyna123 <1224979611@qq.com>
2017-05-04 09:20:45 +0800

    redisUtil工具demo

--- src/main/java/com/rest/TCSL_Rest_Test.java
+++ src/main/java/com/rest/TCSL_Rest_Test.java
@@ -1,6 +1,7 @@
 package com.rest;
 
 import com.bo.TCSL_BO_Test;
+import com.redis.RedisUtil;
 import com.vo.TCSL_VO_Result;
 import com.vo.TCSL_VO_Test;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by zhangtuoyu on 2017/4/27.
@@ -23,25 +24,33 @@ public class TCSL_Rest_Test{
     @Resource(name = "redisTemplate")
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private RedisUtil redisUtil;
 
-    @RequestMapping("helloWorld")
+    @RequestMapping("testList")
     @ResponseBody
-    public String HelloWorld(){
-        return "Hello World!!!!";
-    }
+    public TCSL_VO_Result  testList(){
+        TCSL_VO_Result result = new TCSL_VO_Result();
+        result.setContent("你好");
+        return result;
+//        //清空指定key 所有内容
+//        redisUtil.delCache("testMap");
+//        Object[] values = {"value1","value2","value3"};
+//        redisUtil.addCacheSet("testMap",values);
+//        //查询缓存中内容
+//        Set set = redisUtil.getCacheSet("testMap");
+//        System.out.println("缓存中内容"+set);
+//
+//        Map map = new HashMap();
+//        map.put("value3","修改内容1");
+//        redisUtil.modifyCacheSet("testMap",map);
+//        Set newSet = redisUtil.getCacheSet("testMap");
+//        System.out.println("缓存中修改后的内容"+newSet);
+//
+//        Object[] vals = {"value1"};
+//        redisUtil.delCacheSet("testMap",vals);
+//        Set set2 = redisUtil.getCacheSet("testMap");
+//        System.out.println("缓存中删除后的内容"+set2);
 
-    @RequestMapping("getUsers")
-    @ResponseBody
-    public List<TCSL_VO_Test> getUsers(){
-        List<TCSL_VO_Test> users = tcslBoTest.getUser();
-        return users;
-    }
-
-    @RequestMapping("redis")
-    @ResponseBody
-    public String testRedis(){
-        ValueOperations<String,Object> ops =redisTemplate.opsForValue();
-        ops.set("123","321");
-        return "缓存成功";
     }
 }
